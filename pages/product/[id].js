5bddec152c1388ae954f63488e5da92901756d47
Loghin Dan <loghindan494@gmail.com>
2024-01-06 14:58:34 +0200

    update

--- pages/product/[id].js
+++ pages/product/[id].js
@@ -330,20 +330,22 @@ export default function ProductPage({ product, cartProducts, _id, title: existin
                 )}
                 {/* Image Uploader Input */}
                 <label className="w-24 h-24 cursor-pointer text-center flex flex-col items-center justify-center text-sm gap-1 text-primary rounded-md bg-white shadow-sm border border-primary">
-                  <Icon
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="md:w-10 md:h-10 w-16 h-16 mt-4"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
-                    />
-                  </Icon>
+                  <div className="w-10 h-10 mt-3 flex justify-center">
+                    <Icon
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+                      />
+                    </Icon>
+                  </div>
                   <div className="leading-4">Adaugă imagine</div>
                   <Input type="file" onChange={uploadImages} />
                 </label>
